ff0060a33e8579acf94b6f31fccd9539ee46a5c9
472649340 <3368037498@qq.com>
2023-02-17 19:11:30 +0800

    不连接数据库第一次提交

---
+++ src/test/java/com/jz/mall/JzMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jz.mall;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JzMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
